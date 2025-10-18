58a482ee51b925f215630f7e20fa5bf2387519b5
meuorii <fontillaskian@gmail.com>
2025-10-18 18:31:01 +0800

    remove blink routes in init.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,7 +1,6 @@
 from .admin_routes import *
 from .attendance_routes import *
 from .auth_routes import *
-from .blink_routes import *
 from .face_routes import *
 from .instructor_routes import *
 from .student_routes import *
